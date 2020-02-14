87f7ab90fe5dd87e97d2d16a5ff040896baaca6a
Eric Lifka <eric.lifka@genesys.com>
2020-02-14 11:45:20 -0500

    tie the card buttons to actions on the combat component

--- app/components/combat-view.js
+++ app/components/combat-view.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 import { inject as service } from '@ember/service';
 
 export default class CombatViewComponent extends Component {
@@ -21,6 +22,14 @@ export default class CombatViewComponent extends Component {
     this.playerHand.push(this.cardSet.getCard(0));
     this.playerHand.push(this.cardSet.getCard(0));
     this.playerHand.push(this.cardSet.getCard(0));
+  }
+
+  @action playCard(card) {
+    console.log('playCard', card);
+  }
+
+  @action salvageCard(card) {
+    console.log('salvageCard', card);
   }
 
 }
